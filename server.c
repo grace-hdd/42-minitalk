527b5468ae337fafecb9b208d4d40a758d756af1
grace haddad <gracehadda@gmail.com>
2025-11-06 22:36:40 +0200

    Initial commit: minitalk project

--- server.c
+++ server.c
@@ -5,35 +5,41 @@
 
 void handler(int signum)
 {
-	static int current_char;
-	static int bit_index;
-	
-	current_char <<= 1;
-	if (signum == SIGUSR2)
-		current_char |= 1;
-	bit_index++;
-	if (bit_index == 8)
-	{
-		if (current_char == '\0')
-			write(1, "\n", 1);
-		else
-			write(1, &current_char, 1);
-		current_char = 0;
-		bit_index = 0;
-	}
-
+    static int current_char;
+    static int bit_index;
+    
+    current_char <<= 1;
+    if (signum == SIGUSR2)
+        current_char |= 1;
+    bit_index++;
+    if (bit_index == 8)
+    {
+        if (current_char == '\0')
+            write(1, "\n", 1);
+        else
+            write(1, &current_char, 1);
+        current_char = 0;
+        bit_index = 0;
+    }
 }
 
 int main()
 {
-	struct sigaction sa;
-	int pid = getpid();
-	printf("%d\n", pid);
-	signal(SIGUSR1, handler);
-	signal(SIGUSR2, handler);
-	while (1)
-	{
-		pause();
-	}
-	return (0);
+    struct sigaction sa;
+    int pid = getpid();
+    
+    printf("%d\n", pid);
+    
+    sigemptyset(&sa.sa_mask);
+    sa.sa_handler = handler;
+    sa.sa_flags = SA_RESTART;
+    
+    sigaction(SIGUSR1, &sa, NULL);
+    sigaction(SIGUSR2, &sa, NULL);
+    
+    while (1)
+    {
+        pause();
+    }
+    return (0);
 }
